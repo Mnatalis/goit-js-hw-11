c3c7198a18e7946058cb81f9403809ed87090a8c
Nataliss <oksklient@gmail.com>
2023-05-07 18:12:10 +0100

    scroll update

--- src/index.js
+++ src/index.js
@@ -1,45 +1,115 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
-import Notiflix, { Notify } from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import { fetchData } from './js/fetchData';
-
+const form = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
-const form = document.querySelector('.search-form');
-const loadMoreBtn = document.querySelector('.loadMoreBtn');
-loadMoreBtn.addEventListener('click', moreBtn);
+// const loadButton = document.querySelector('.load-more');
+const guard = document.querySelector('.guard');
 
-let perPage = 40;
+const perPage = 40;
 let page = 1;
-let maxPage = 0;
-let query = '';
+let keyWord = '';
 
-form.addEventListener('submit', async event => {
-  query = event.target.elements.searchQuery.value;
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        onSerch(keyWord);
+      }
+    });
+  },
+  { rootMargin: '200px' }
+);
+
+form.addEventListener('submit', handSubmit);
+
+function handSubmit(event) {
   event.preventDefault();
-  removeItems();
-
-  let inputFormValue = query.toLowerCase().trim();
-
-  if (inputFormValue === '') {
+  const inputValue = form.searchQuery.value.trim();
+  if (!inputValue || keyWord === inputValue) {
     return;
   }
-  fetchData(query, perPage, page).then(checkSearchData);
+  keyWord = inputValue;
+  page = 1;
+  gallery.innerHTML = '';
+  // loadButton.classList.add('is-hidden');
+  onSerch(keyWord, page);
+}
 
-  markupContent;
-});
+// loadButton.addEventListener('click', () => {
+//   loadButton.classList.add('is-hidden');
 
-let galleryLightbox = new SimpleLightbox('.photo-card a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-  captionPosition: 'bottom',
-});
+//   onSerch(keyWord);
+//   smoothScroll();
+// });
 
-function markupContent(data) {
-  const markup = data.hits
-    .map(
-      ({
+// function onSerch(keyWord) {
+//   getResponse(keyWord)
+//     .then(response => {
+//       const totalPages = Math.ceil(response.totalHits / perPage);
+
+//       if (page == 1) {
+//         Notify.success(`Hooray! We found ${response.totalHits} images.`);
+//       }
+
+//       renderGallery(response.hits);
+//       if (page > 1) {
+//         smoothScroll();
+//       }
+//       page += 1;
+
+//       const lightbox = new SimpleLightbox('.gallery a');
+//       lightbox.refresh();
+
+//       if (page > totalPages) {
+//         Notify.warning(
+//           "We're sorry, but you've reached the end of search results."
+//         );
+//         loadButton.classList.add('is-hidden');
+//       }
+//     })
+//     .catch(error => {
+//       Notify.failure(error.message);
+//     });
+// }
+
+async function onSerch(keyWord) {
+  const data = await getResponse(keyWord);
+
+  const totalPages = Math.ceil(data.totalHits / perPage);
+  console.log(totalPages);
+  if (page == 1) {
+    Notify.success(`Hooray! We found ${data.totalHits} images.`);
+  }
+
+  renderGallery(data.hits);
+
+  if (page > 1) {
+    smoothScroll();
+  }
+  page += 1;
+
+  const lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
+  observer.observe(guard);
+
+  if (page > totalPages) {
+    Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+    observer.unobserve(guard);
+    return;
+    // loadButton.classList.add('is-hidden');
+  }
+}
+
+function renderGallery(fotos) {
+  // loadButton.classList.add('is-hidden');
+  const markupGallery = fotos
+    .map(foto => {
+      const {
         webformatURL,
         largeImageURL,
         tags,
@@ -47,70 +117,74 @@ function markupContent(data) {
         views,
         comments,
         downloads,
-      }) => {
-        return `<div class="photo-card">
-        <a class="gallery__item" href="${largeImageURL}">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-        </a>
-        <div class="info">
-          <p class="info-item">
-            <b>Likes ${likes}</b>
-          </p>
-          <p class="info-item">
-            <b>Views ${views}</b>
-          </p>
-          <p class="info-item">
-            <b>Comments ${comments}</b>
-          </p>
-          <p class="info-item">
-            <b>Downloads ${downloads}</b>
-          </p>
-        </div>
-      </div>`;
-      }
-    )
+      } = foto;
+      return `<div class="photo-card">
+  <a class="gallery-link" href=${largeImageURL}><img   class="gallery-image" src=${webformatURL} alt=${tags} loading="lazy" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b> ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b> ${views}
+    </p>
+      <p class="info-item">
+      <b>Comments</b> ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b> ${downloads}
+    </p>
+  </div>
+</div>`;
+    })
     .join('');
-
-  gallery.insertAdjacentHTML('beforeend', markup);
+  gallery.insertAdjacentHTML('beforeend', markupGallery);
+  // loadButton.classList.remove('is-hidden');
 }
 
-function removeItems() {
-  page = 1;
-  gallery.innerHTML = '';
-  loadMoreBtn.classList.add('hidden');
-}
+// function getResponse(name) {
+//   const API_KEY = `33587680-716f90511d290523ac71bcbd3`;
 
-function checkSearchData(search) {
-  // console.log(search.total);
+//   return axios
+//     .get(
+//       `https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+//     )
+//     .then(response => {
+//       if (response.data.totalHits === 0) {
+//         throw Error(
+//           'Sorry, there are no images matching your search query. Please try again.'
+//         );
+//       }
 
-  const total = search.total;
+//       return response.data;
+//     });
+// }
 
-  if (total > 0) {
-    loadMoreBtn.classList.remove('hidden');
-
-    Notiflix.Notify.success(`We have the ${total} pictures fo you!`);
-    markupContent(search);
-    galleryLightbox.refresh();
-  }
-
-  if (total === 0) {
-    loadMoreBtn.classList.add('hidden');
-    Notiflix.Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
+async function getResponse(name) {
+  try {
+    const API_KEY = `33587680-716f90511d290523ac71bcbd3`;
+    const { data } = await axios.get(
+      `https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
     );
-    removeItems();
-  }
 
-  if (total > perPage) {
-    loadMoreBtn.classList.remove('hidden');
-  }
-  if (maxPage <= page) {
-    loadMoreBtn.classList.add('hidden');
+    if (data.totalHits === 0) {
+      throw Error(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+
+    return data;
+  } catch (error) {
+    Notify.failure(error.message);
   }
 }
 
-function moreBtn() {
-  page += 1;
+function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
 
-  fetchData(query, perPage, page).then(checkSearchData);
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
